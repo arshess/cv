500def3b9580ac39f3117a89b15947817115b257
arshess <s3th80@gmail.com>
2021-01-31 23:51:02 +0100

    ajouts divers

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,8 @@ $(function(){
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl);    
     });
-    var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
-        trigger: 'focus'
-    })
-
-
-    var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
-        container: 'body'
-      })
-      
     var discord = $('#tocopy');
     $('#btn-discord').click(function(){
-        navigator.clipboard.writeText('Arshess#1700').then(function(){
-            // $('#link-Discord').attr('data-bs-toggle',"tooltip");
-            // $('#link-Discord').attr('data-bs-placement',"top");
-            // $('#link-Discord').attr('title',"Copié !");
-            
-            
-        });
-          
-        }
-        
-      );
-
-    // navigator.clipboard.writeText().then(text => outputElem.innerText = text);
-
-
+        navigator.clipboard.writeText('Arshess#1700');   
+    });      
 });
